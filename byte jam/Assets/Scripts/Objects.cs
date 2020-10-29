c73e81907a5b315c4b37da409a1f091ec84c0b18
kkiser <21kkiser@macon.k12.mo.us>
2020-10-29 09:29:42 -0500

    Main Scene

---
+++ byte jam/Assets/Scripts/Objects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * Random.Range(12,13),ForceMode.Impulse);
+        rb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10),ForceMode.Impulse);
+    }
+
+
+}
--- byte jam/Assets/Scripts/SpawnManager.cs
+++ byte jam/Assets/Scripts/SpawnManager.cs
@@ -6,29 +6,32 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] fruitPrefabs;
 
-    private float spawnLimitXLeft = -1;
-    private float spawnLimitXRight = 1;
-    private float spawnPosY = 3;
+    private float spawnLimitXLeft = -1.5f;
+    private float spawnLimitXRight = 1.5f;
+    private float spawnPosY = -3;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = 1.5f;
     public float thrust = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomFruit", startDelay, spawnInterval);
+        StartCoroutine("SpawnRandomFruit");
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomFruit()
+    public IEnumerator SpawnRandomFruit()
     {
         // Generate random ball index and random spawn position
-        int num = Random.Range(0, 3);
+        int num = Random.Range(0, 6);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, -3);
 
         // instantiate ball at random spawn location
         Instantiate(fruitPrefabs[num], spawnPos, fruitPrefabs[num].transform.rotation);
+        float time = Random.Range(.5f,3f);
+        yield return new WaitForSeconds(time);
+        StartCoroutine("SpawnRandomFruit");
     }
 
 }
